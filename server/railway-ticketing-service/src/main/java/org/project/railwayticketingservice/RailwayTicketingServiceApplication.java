685d8cadfc79e33140518cb6fed7544bf72eeccf
Isla <islarcherjr@icloud.com>
2025-07-27 22:01:31 +0100

    backend: first commit

---
+++ server/railway-ticketing-service/src/main/java/org/project/railwayticketingservice/RailwayTicketingServiceApplication.java
@@ -0,0 +1,13 @@
+package org.project.railwayticketingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayTicketingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailwayTicketingServiceApplication.class, args);
+    }
+
+}

a47a5d5f74b06534cb9208545253cb6e29eeb178
Isla <islarcherjr@icloud.com>
2025-07-28 23:55:08 +0100

    backend: fleshed out login dtos.

--- server/railway-ticketing-service/src/main/java/org/project/railwayticketingservice/dto/auth/request/LoginAdminRequest.java
+++ server/railway-ticketing-service/src/main/java/org/project/railwayticketingservice/dto/auth/request/LoginAdminRequest.java
@@ -1,4 +1,19 @@
 package org.project.railwayticketingservice.dto.auth.request;
 
-public record LoginAdminRequest() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record LoginAdminRequest(
+        @NotNull
+        @NotBlank
+        @Email(message = "please provide a valid e-mail address")
+        @Size(max = 80, message = "e-mail cannot be more than 80 characters")
+        String email,
+
+        @NotNull(message = "password field cannot be null")
+        @NotBlank(message = "please provide a password")
+        @Size(max = 60, message = "password cannot be more than 60 characters")
+        String password) {
 }
--- server/railway-ticketing-service/src/main/java/org/project/railwayticketingservice/dto/auth/request/LoginPassengerRequest.java
+++ server/railway-ticketing-service/src/main/java/org/project/railwayticketingservice/dto/auth/request/LoginPassengerRequest.java
@@ -1,4 +1,20 @@
 package org.project.railwayticketingservice.dto.auth.request;
 
-public record LoginPassengerRequest() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record LoginPassengerRequest(
+        @NotNull
+        @NotBlank
+        @Email(message = "please provide a valid e-mail address")
+        @Size(max = 80, message = "e-mail cannot be more than 80 characters")
+        String email,
+
+        @NotNull(message = "password field cannot be null")
+        @NotBlank(message = "please provide a password")
+        @Size(max = 60, message = "password cannot be more than 60 characters")
+        String password
+) {
 }

4d18845e04a1d153f722eb49c400086562f77696
Isla <islarcherjr@icloud.com>
2025-07-29 00:12:06 +0100

    backend: fleshed out register dtos.

--- server/railway-ticketing-service/src/main/java/org/project/railwayticketingservice/dto/auth/request/RegisterAdminRequest.java
+++ server/railway-ticketing-service/src/main/java/org/project/railwayticketingservice/dto/auth/request/RegisterAdminRequest.java
@@ -1,4 +1,30 @@
 package org.project.railwayticketingservice.dto.auth.request;
 
-public record RegisterAdminRequest() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record RegisterAdminRequest(
+        @NotNull(message = "please provide a valid firstName")
+        @NotBlank(message = "firstName field cannot be blank")
+        @Size(max = 50, message = "firstName cannot be more than 50 characters")
+        String firstName,
+
+        @NotNull(message = "please provide a valid lastName")
+        @NotBlank(message = "lastName field cannot be blank")
+        @Size(max = 50, message = "lastName cannot be more than 50 characters")
+        String lastName,
+
+        @NotNull(message = "please provide an e-mail address")
+        @NotBlank(message = "e-mail address field cannot be blank")
+        @Email(message = "please provide a valid e-mail address")
+        @Size(max = 80, message = "e-mail address cannot be more than 80 characters")
+        String email,
+
+        @NotNull(message = "please provide a valid password")
+        @NotBlank(message = "password field cannot be blank")
+        @Size(max = 60, message = "password cannot be more than 60 characters")
+        String password
+) {
 }
--- server/railway-ticketing-service/src/main/java/org/project/railwayticketingservice/dto/auth/request/RegisterPassengerRequest.java
+++ server/railway-ticketing-service/src/main/java/org/project/railwayticketingservice/dto/auth/request/RegisterPassengerRequest.java
@@ -1,6 +1,40 @@
 package org.project.railwayticketingservice.dto.auth.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record RegisterPassengerRequest(
 
+    @NotNull(message = "please provide a valid firstName")
+    @NotBlank(message = "firstName field cannot be blank")
+    @Size(max = 50, message = "firstName cannot be more than 50 characters")
+    String firstName,
+
+    @NotNull(message = "please provide a valid lastName")
+    @NotBlank(message = "lastName field cannot be blank")
+    @Size(max = 50, message = "lastName cannot be more than 50 characters")
+    String lastName,
+
+    @NotNull(message = "please provide an e-mail address")
+    @NotBlank(message = "e-mail address field cannot be blank")
+    @Email(message = "please provide a valid e-mail address")
+    @Size(max = 80, message = "e-mail address cannot be more than 80 characters")
+    String email,
+
+    @NotNull(message = "please input a proper password")
+    @NotBlank(message = "password field cannot be blank")
+    @Size(max = 60, message = "password cannot be more than 60 characters")
+    String password,
+
+    @NotNull(message = "please provide a valid country")
+    @NotBlank(message = "'country' field cannot be blank")
+    String country,
+
+    @NotNull(message = "please provide a phone number")
+    @NotBlank(message = "phone number field cannot be blank")
+    @Size(max = 11, message = "please input your phone number in the format: (08023456789)")
+    String phone
 ) {
 }

005111792074ad7f14d447026bf09c4d8a44c873
Isla <islarcherjr@icloud.com>
2025-08-07 23:39:10 +0100

    backend: update - added proper swagger docs for new station method.

--- server/railway-ticketing-service/src/main/java/org/project/railwayticketingservice/controller/StationController.java
+++ server/railway-ticketing-service/src/main/java/org/project/railwayticketingservice/controller/StationController.java
@@ -1,5 +1,8 @@
 package org.project.railwayticketingservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.project.railwayticketingservice.dto.app.request.NewStationRequest;
@@ -18,6 +21,15 @@ public class StationController {
 
     private final StationService stationService;
 
+    @Operation(description = "This endpoint creates a new train station.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successfully created the train station"),
+            @ApiResponse(responseCode = "500", description = "Internal Error creating train station"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized access"),
+            @ApiResponse(responseCode = "409", description = "Train station already exists"),
+            @ApiResponse(responseCode = "400", description = "Bad request")
+
+    })
     @PostMapping("/new")
     public ResponseEntity<StationResponse> createStation(NewStationRequest request) {
         return stationService.createStation(request);

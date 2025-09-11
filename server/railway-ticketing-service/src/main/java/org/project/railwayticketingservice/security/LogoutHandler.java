baeb85a61cd676794865b94da179c0237e06514e
Isla <islarcherjr@icloud.com>
2025-09-11 22:45:40 +0100

    backend: fix - fixed logout problem

--- server/railway-ticketing-service/src/main/java/org/project/railwayticketingservice/security/LogoutHandler.java
+++ server/railway-ticketing-service/src/main/java/org/project/railwayticketingservice/security/LogoutHandler.java
@@ -4,23 +4,34 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.project.railwayticketingservice.exception.exceptions.RtsException;
+import org.project.railwayticketingservice.repository.RefreshTokenRepository;
+import org.project.railwayticketingservice.service.CustomUserDetailsService;
+import org.project.railwayticketingservice.service.JwtService;
 import org.project.railwayticketingservice.service.TokenService;
 import org.project.railwayticketingservice.util.CookieUtils;
 import org.project.railwayticketingservice.util.Utilities;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+
 @Component
+@Transactional
 @RequiredArgsConstructor
 public class LogoutHandler implements org.springframework.security.web.authentication.logout.LogoutHandler {
 
     private final TokenService tokenService;
     private final CookieUtils cookieUtils;
     private final Utilities utilities;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtService jwtService;
+    private final CustomUserDetailsService customUserDetailsService;
 
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
@@ -31,19 +42,21 @@ public class LogoutHandler implements org.springframework.security.web.authentic
             String token = authHeader.substring(AUTH_PREFIX.length());
             if (tokenService.isTokenAllowed(token)) {
                 tokenService.disallowToken(token);
-                System.out.println("token disallowed for user " + authentication.getName());
+                System.out.println("token disallowed for e-mail " + jwtService.extractEmail(token));
             } else {
                 throw new RtsException(HttpStatus.UNAUTHORIZED, "token is disallowed!");
             }
             // invalidate refresh token
             Cookie clearRefreshCookie;
-            if (utilities.isAdmin(authentication)) {
-                System.out.println("clearing cookies for admin " + authentication.getName());
+            String email = jwtService.extractEmail(token);
+            if (customUserDetailsService.loadUserByUsername(email).getAuthorities().equals(Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN")))) {
+                System.out.println("clearing admin cookies for e-mail: " + email);
                 clearRefreshCookie = cookieUtils.clearRefreshTokenCookie("admin");
             } else {    // if it's a passenger
-                System.out.println("clearing cookies for passenger " + authentication.getName());
+                System.out.println("clearing cookies for passenger e-mail: " + email);
                 clearRefreshCookie = cookieUtils.clearRefreshTokenCookie("passenger");
             }
+            refreshTokenRepository.deleteRefreshTokenByEmail(email);
             response.addCookie(clearRefreshCookie);
         } else {
             throw new RtsException(HttpStatus.BAD_REQUEST, "failed to logout!");
@@ -53,7 +66,7 @@ public class LogoutHandler implements org.springframework.security.web.authentic
         HttpSession session = request.getSession(false);
         if (session != null) {
             session.invalidate();
-            System.out.println("session invalidated for user " + authentication.getName());
+            System.out.println("session invalidated for user");
         }
 
         // clear security context

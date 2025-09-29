2b11a9c58fcb68eb3660e2709ee37a13e9a45636
Isla <islarcherjr@icloud.com>
2025-09-29 23:30:34 +0100

    backend: update - initialized connectionFactory

--- server/railway-ticketing-service/src/main/java/org/project/railwayticketingservice/config/CacheConfig.java
+++ server/railway-ticketing-service/src/main/java/org/project/railwayticketingservice/config/CacheConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -15,6 +17,11 @@ import java.time.Duration;
 @EnableCaching
 @Configuration
 public class CacheConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("localhost", 6379));
+    }
 
     @Bean
     public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
